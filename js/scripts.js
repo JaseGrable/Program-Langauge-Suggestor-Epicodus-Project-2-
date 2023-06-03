05a1943f61bab90ef56c8dc9a44da8a27bf6a329
JaseGrable <128560337+JaseGrable@users.noreply.github.com>
2023-06-03 10:36:17 -0700

    WIP javascript click function

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,9 @@ window.onload = function () {
     const question4 = document.getElementById('question4');
     const question5 = document.getElementById('question5');
     const resultSection = document.getElementById('result');
-    const resultPython = document.getElementById('pythonResult');
-    const resultJava = document.getElementById('javaResult');
-    const resultC = document.getElementById('cResult');
+    const resultPython = document.getElementById('resultPython');
+    const resultJava = document.getElementById('resultJava');
+    const resultC = document.getElementById('resultC');
     const resultRetry = document.getElementById('resultRetry');
     const refreshButton = document.getElementById('refreshButton');
 
@@ -16,7 +16,7 @@ window.onload = function () {
         question1.classList.remove('hidden');
 
         const answerInputs = question1.querySelectorAll('input[type="radio"]');
-        answerInputs.forEach(function (input)) {
+        answerInputs.forEach(function (input) {
             input.addEventListener('change', function () {
                 const selectedValue = input.value;
 
@@ -25,7 +25,114 @@ window.onload = function () {
                 } else {
                     showQuestion2();
                 }
-            })
-        }
+            });
+        });
     }
-}
+
+    function showQuestion2() {
+        question2.classList.remove('hidden');
+
+        const answerInputs = question2.querySelectorAll('input[type="radio"]');
+        answerInputs.forEach(function (input) {
+            input.addEventListener('change', function () {
+                const selectedValue = input.value;
+
+                if (selectedValue === 'yes') {
+                    showResultJava();
+                } else {
+                    showQuestion3();
+                }
+            });
+        });
+    }
+
+    function showQuestion3() {
+        question3.classList.remove('hidden');
+
+        const answerInputs = question3.querySelectorAll('input[type="radio"]');
+        answerInputs.forEach(function (input) {
+            input.addEventListener('change', function () {
+                const selectedValue = input.value;
+
+                if (selectedValue === 'yes') {
+                    showResultC();
+                } else {
+                    showQuestion4();
+                }
+            });
+        });
+    }
+
+    function showQuestion4() {
+        question4.classList.remove('hidden');
+
+        const answerInputs = question4.querySelectorAll('input[type="radio"]');
+        answerInputs.forEach(function (input) {
+            input.addEventListener('change', function () {
+                const selectedValue = input.value;
+
+                if (selectedValue === 'yes') {
+                    showResultJava();
+                } else {
+                    showQuestion5();
+                }
+            });
+        });
+    }
+
+    function showQuestion5() {
+        question5.classList.remove('hidden');
+
+        const answerInputs = question5.querySelectorAll('input[type="radio"]');
+        answerInputs.forEach(function (input) {
+            input.addEventListener('change', function () {
+                const selectedValue = input.value;
+
+                if (selectedValue === 'yes') {
+                    showResultC();
+                } else {
+                    showResultRetry();
+                }
+            });
+        });
+    }
+
+    function showResultPython() {
+        resultSection.classList.remove('hidden');
+        resultPython.classList.remove('hidden');
+        refreshButton.classList.remove('hidden');
+    }
+
+    function showResultJava() {
+        resultSection.classList.remove('hidden');
+        resultJava.classList.remove('hidden');
+        refreshButton.classList.remove('hidden');
+    }
+
+    function showResultC() {
+        resultSection.classList.remove('hidden');
+        resultC.classList.remove('hidden');
+        refreshButton.classList.remove('hidden');
+    }
+
+    function showResultRetry() {
+        resultSection.classList.remove('hidden');
+        resultRetry.classList.remove('hidden');
+        refreshButton.classList.remove('hidden');
+    }
+
+    function resetForm() {
+        questionForm.reset();
+        resultSection.classList.add('hidden');
+        resultPython.classList.add('hidden');
+        resultJava.classList.add('hidden');
+        resultC.classList.add('hidden');
+        resultRetry.classList.add('hidden');
+        refreshButton.classList.add('hidden');
+        showQuestion1();
+    }
+
+    refreshButton.addEventListener('click', resetForm);
+
+    resetForm();
+};
